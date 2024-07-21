b725baa78b2f2656c4968203cecec15854f13a51
Eric Eng <epe24@cornell.edu>
2024-07-21 07:18:37 -0400

    added configs and contexts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
-import { BrowserRouter as Router } from 'react-router-dom';  // Import Router if it's not yet imported in App
-import { AuthProvider } from './contexts/AuthContext';  // Ensure the path matches where the AuthProvider is defined
+import { AuthProvider } from './contexts/AuthContext';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
